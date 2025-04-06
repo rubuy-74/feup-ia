b8a64fc108b19b8bf53a855306cc6bdc15c3ebe6
DGoiana <diogogoianam@gmail.com>
2025-04-06 11:53:38 +0100

    fixing hillclimb v2.0

--- proj/src/algorithms/hillclimb.py
+++ proj/src/algorithms/hillclimb.py
@@ -7,7 +7,7 @@ def hillclimb(m: Map, remaining_budget: int, it: int):
   temp = copy.deepcopy(m)
   solutions = []
   
-  pbar = tqdm(range(it), desc="Running Hill Climb")
+  #pbar = tqdm(range(it), desc="Running Hill Climb")
   
   for _ in range(it):
     new_map, new_budget, _ = mutation_func(m, remaining_budget)
@@ -15,16 +15,18 @@ def hillclimb(m: Map, remaining_budget: int, it: int):
     new_map_value = new_map.evaluate(new_budget)
     old_map_value = temp.evaluate(remaining_budget) 
     
+    print(old_map_value, new_map_value)
+    
     if(new_map_value > old_map_value):
+      print("ACCEPTING BEST")
       solutions.append(new_map_value)
+      temp = copy.deepcopy(new_map)
       
-      temp = copy.deepcopy(m)
-      
-      remaining_budget = new_budget
-    
     m = new_map
-    pbar.update()
+    remaining_budget = new_budget
     
-  pbar.close()      
+    #pbar.update()
+    
+  #pbar.close()      
 
   return temp, remaining_budget,solutions
